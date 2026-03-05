f98fa958d1fc37157ae1267a417e97db12729ca0
Deekshitha <deekshitharavindran7@gmail.com>
2026-03-05 17:43:11 +0530

    Initial commit without .terraform directory

---
+++ main.tf
@@ -0,0 +1,114 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.static_site.id
+  key          = "index.html"
+  source       = "website/index.html"
+  content_type = "text/html"
+  etag         = filemd5("website/index.html")
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.static_site.id
+  key          = "error.html"
+  source       = "website/error.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "style" {
+  bucket       = aws_s3_bucket.static_site.id
+  key          = "style.css"
+  source       = "website/style.css"
+  content_type = "text/css"
+}
+resource "aws_s3_bucket_public_access_block" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.static_site.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.static_site
+  ]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.static_site.arn}/*"
+    }]
+  })
+}
+resource "aws_cloudfront_distribution" "static_site" {
+
+  origin {
+    domain_name = aws_s3_bucket.static_site.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  price_class         = "PriceClass_100"
+
+  default_cache_behavior {
+
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+}
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
